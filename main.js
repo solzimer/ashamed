aa256fd5d67f491e63925aabf9f97bca3aa122c0
solzimer <solzimer@gmail.com>
2017-06-20 17:31:56 +0200

    Refactor, model update aware
    Now, model is real-time updated if subscribed

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const cluster = require('cluster');
 
 if(cluster.isMaster) {
-	module.exports = require('./master.js');
+	module.exports = require('./lib/master.js');
 }
 else {
-	module.exports = require('./worker.js');
+	module.exports = require('./lib/worker.js');
 }
